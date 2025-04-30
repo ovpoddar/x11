6c45778e65864dc3a418130b460cbb9be0ca8835
Ov poddar Windows <ayanpoddar0@gmail.com>
2025-04-30 12:53:55 +0530

    working on event

---
+++ src/Models/XAnyEvent.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace X11cs.Models;
+
+[StructLayout(LayoutKind.Sequential)]
+public unsafe struct XAnyEvent
+{
+    public Event Type;
+    public ulong Serial;   /* # of last request processed by server */
+    public bool SendEvent;    /* true if this came from a SendEvent request */
+    public Display* Display;/* Display the event was read from */
+    public ulong Window;	/* window on which event was requested in event mask */
+}
---
+++ src/Models/XModifierKeymap.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.CompilerServices.RuntimeHelpers;
+
+namespace X11cs.Models;
+
+[StructLayout(LayoutKind.Sequential)]
+public unsafe struct XModifierKeymap
+{
+    public int MaxKeyPerMod;  /* The server's max # of keys per modifier */
+    public byte* ModifierMap;	/* An 8 by max_keypermod array of modifiers */
+}
