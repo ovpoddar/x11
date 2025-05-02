bc1c55295a357cef22fd76607976cfd3021f5fcd
Ov poddar Windows <ayanpoddar0@gmail.com>
2025-05-02 21:11:45 +0530

    Add X11 reply structures to X11cs.Models namespace
    This commit introduces a series of new structures in the `X11cs.Models` namespace, representing various types of replies from the X11 protocol. Each structure is defined with the `StructLayout` attribute for proper memory layout, ensuring compatibility with native code. The new structures include `XCharInfo`, `XGenericReply`, `XGetAtomNameReply`, among others, with fields of types such as `byte`, `ushort`, `uint`, and `short` to match the expected data from the X11 server.
    
    Additionally, the `xReply` structure has been modified to support overlapping fields, allowing it to represent different reply types based on the request context, utilizing the `FieldOffset` attribute for precise field positioning.

---
+++ src/Models/XGrabPointerReply.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.InteropServices;
+
+namespace X11cs.Models;
+
+[StructLayout(LayoutKind.Sequential)]
+public struct XGrabPointerReply
+{
+    public byte Type; /* X_Reply */
+    public byte Status;
+    public ushort SequenceNumber;
+    public uint Length; /* 0 */
+    public uint Pad1;
+    public uint Pad2;
+    public uint Pad3;
+    public uint Pad4;
+    public uint Pad5;
+    public uint Pad6;
+}
---
+++ src/Models/xReply.cs
@@ -0,0 +1,84 @@
+﻿using System.Runtime.InteropServices;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace X11cs.Models;
+[StructLayout(LayoutKind.Explicit, Size = 32)]
+public struct xReply
+{
+    [FieldOffset(0)]
+    public XGenericReply Generic;
+    [FieldOffset(0)]
+    public XGetGeometryReply Geom;
+    [FieldOffset(0)] 
+    public XQueryTreeReply Tree;
+    [FieldOffset(0)]
+    public XInternAtomReply Atom;
+    [FieldOffset(0)]
+    public XGetAtomNameReply AtomName;
+    [FieldOffset(0)]
+    public XGetPropertyReply Property;
+    [FieldOffset(0)]
+    public XListPropertiesReply ListProperties;
+    [FieldOffset(0)]
+    public XGetSelectionOwnerReply Selection;
+    [FieldOffset(0)]
+    public XGrabPointerReply GrabPointer;
+    [FieldOffset(0)]
+    public xGrabKeyboardReply GrabKeyboard;
+    [FieldOffset(0)]
+    public XQueryPointerReply Pointer;
+    [FieldOffset(0)]
+    public XGetMotionEventsReply MotionEvents;
+    [FieldOffset(0)]
+    public XTranslateCoordsReply Coords;
+    [FieldOffset(0)]
+    public XGetInputFocusReply InputFocus;
+    [FieldOffset(0)]
+    public XQueryTextExtentsReply TextExtents;
+    [FieldOffset(0)]
+    public XListFontsReply Fonts;
+    [FieldOffset(0)]
+    public XGetFontPathReply FontPath;
+    [FieldOffset(0)]
+    public xGetImageReply Image;
+    [FieldOffset(0)]
+    public xListInstalledColormapsReply Colormaps;
+    [FieldOffset(0)]
+    public xAllocColorReply AllocColor;
+    [FieldOffset(0)]
+    public xAllocNamedColorReply AllocNamedColor;
+    [FieldOffset(0)]
+    public xAllocColorCellsReply ColorCells;
+    [FieldOffset(0)]
+    public xAllocColorPlanesReply ColorPlanes;
+    [FieldOffset(0)]
+    public xQueryColorsReply Colors;
+    [FieldOffset(0)]
+    public xLookupColorReply LookupColor;
+    [FieldOffset(0)]
+    public xQueryBestSizeReply BestSize;
+    [FieldOffset(0)]
+    public xQueryExtensionReply Extension;
+    [FieldOffset(0)]
+    public xListExtensionsReply Extensions;
+    [FieldOffset(0)]
+    public xSetModifierMappingReply SetModifierMapping;
+    [FieldOffset(0)]
+    public xGetModifierMappingReply GetModifierMapping;
+    [FieldOffset(0)]
+    public xSetPointerMappingReply SetPointerMapping;
+    [FieldOffset(0)]
+    public xGetKeyboardMappingReply GetKeyboardMapping;
+    [FieldOffset(0)]
+    public xGetPointerMappingReply GetPointerMapping;
+    [FieldOffset(0)]
+    public xGetPointerControlReply PointerControl;
+    [FieldOffset(0)]
+    public xGetScreenSaverReply ScreenSaver;
+    [FieldOffset(0)]
+    public xListHostsReply Hosts;
+    [FieldOffset(0)]
+    public XError Error;
+    [FieldOffset(0)]
+    public XEvent Event;
+}
